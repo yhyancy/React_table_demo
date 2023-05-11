59b04265f1101b03b022ed047fa04f8ad4232184
yhyancy <2397856503@qq.com>
2023-05-11 14:41:19 +0800

    implement basic table

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import Table from "./components/Table.tsx";
+
+const columns = [
+  { header: 'ID', accessor: 'id', sortable: true },
+  { header: 'Name', accessor: 'name' },
+  { header: 'Age', accessor: 'age', sortable: true },
+  { header: 'Email', accessor: 'email' },
+];
+
+const data = [
+  { id: 1, name: 'Alice', age: 25, email: 'alice@example.com' },
+  { id: 2, name: 'Bob', age: 30, email: 'bob@example.com' },
+  { id: 3, name: 'Charlie', age: 20, email: 'charlie@example.com' },
+  { id: 4, name: 'David', age: 35, email: 'david@example.com' },
+  { id: 5, name: 'Eve', age: 28, email: 'eve@example.com' },
+  { id: 6, name: 'Frank', age: 22, email: 'frank@example.com' },
+];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Table data={data} columns={columns} />
     </div>
   );
 }
---
+++ src/components/Table.tsx
@@ -0,0 +1,58 @@
+import React from "react";
+
+interface TableColumn {
+  header: string;
+  accessor: string;
+  sortable?: boolean;
+}
+
+interface TableProps {
+  columns: TableColumn[];
+  data: any[];
+  pageSize?: number;
+  leftFixedColumns?: number;
+  rightFixedColumns?: number;
+}
+
+enum SortOrder {
+  ASCENDING = "ASC",
+  DESCENDING = "DESC",
+}
+
+export default function Table({ columns, data }: TableProps) {
+  const renderTableHeader = () => {
+    return (
+      <thead>
+        <tr>
+          {columns.map((column) => (
+            <th key={column.accessor}>{column.header}</th>
+          ))}
+        </tr>
+      </thead>
+    );
+  };
+
+  const renderTableBody = () => {
+    return (
+      <tbody>
+        {data.map((row) => (
+          <tr key={row.id}>
+            {columns.map((column) => (
+              <td key={column.accessor}>{row[column.accessor]}</td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+    );
+  };
+
+  return (
+    <div className="table-container">
+      <table border={2}>
+        {renderTableHeader()}
+        {renderTableBody()}
+      </table>
+      {/* Render the pagination UI */}
+    </div>
+  );
+}
